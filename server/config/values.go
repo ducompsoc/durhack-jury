a14373f484c4c907cad33f9bf9ff8fc1b79138fd
Luca <30503695+tameTNT@users.noreply.github.com>
2024-10-14 22:58:09 +0100

    Make note about realm structure changes
    Future additions to the codebase to request additional user details
    will require use of the admin realm.
    This is not yet implemented but a note has been made.

--- server/config/values.go
+++ server/config/values.go
@@ -5,5 +5,7 @@ var (
 	Origin         = GetOptEnv("ORIGIN", "http://jury.durhack-dev.com")
 	ApiOrigin      = GetOptEnv("API_ORIGIN", "http://jury.durhack-dev.com")
 	KeycloakIssuer = "https://auth.durhack.com/realms/durhack-dev"
-	ClientID       = GetEnv("KEYCLOAK_OAUTH2_CLIENT_ID")
+	// lucatodo: accomodate additional realm structure (i.e. admin)
+	// https://github.com/ducompsoc/durhack/blob/130a71ab674288cbe1a6e0e2f3a518773658bc9f/server/src/config/default.ts#L22C3-L30C5
+	ClientID = GetEnv("KEYCLOAK_OAUTH2_CLIENT_ID")
 )
