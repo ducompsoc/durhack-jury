439a3420abbd6f163f59d00dbe81275c43c34149
Luca <30503695+tameTNT@users.noreply.github.com>
2024-10-26 16:48:50 +0100

    🐛 Actually fix the previously mentioned bug
    A* commits only here!

--- server/config/values.go
+++ server/config/values.go
@@ -1,11 +1,30 @@
 package config
 
-var (
-	Port           = GetOptEnv("PORT", "3301")
-	Origin         = GetOptEnv("ORIGIN", "http://jury.durhack-dev.com")
-	ApiOrigin      = GetOptEnv("API_ORIGIN", "http://jury.durhack-dev.com")
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+var ( // this is run first before init
+	Port           string
+	Origin         string
+	ApiOrigin      string
+	KeycloakIssuer string
+	ClientID       string
+)
+
+func init() {
+	// Load .env file into system environment variables which are then picked up below
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Did not load .env file (%s). This is expected when running in a Docker container\n", err.Error())
+	}
+
+	Port = GetOptEnv("PORT", "3301")
+	Origin = GetOptEnv("ORIGIN", "http://jury.durhack-dev.com")
+	ApiOrigin = GetOptEnv("API_ORIGIN", "http://jury.durhack-dev.com")
 	KeycloakIssuer = "https://auth.durhack.com/realms/durhack-dev"
 	// lucatodo: accomodate additional realm structure (i.e. admin) to get user names without hard saving in Flag db etc.
 	// https://github.com/ducompsoc/durhack/blob/130a71ab674288cbe1a6e0e2f3a518773658bc9f/server/src/config/default.ts#L22C3-L30C5
 	ClientID = GetEnv("KEYCLOAK_OAUTH2_CLIENT_ID")
-)
+}
--- server/main.go
+++ server/main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
 	"server/config"
 	"server/database"
 	"server/router"
 )
 
-func init() {
-	// Load the env file
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Printf("Did not load .env file (%s). This is expected when running in a Docker container\n", err.Error())
-	}
-}
-
 func main() {
-
 	// Check for all necessary env variables
 	config.CheckEnv()
 
