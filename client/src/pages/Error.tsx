ea6e013d596f83e77d35125cebbda7273cf37e22
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-11-03 16:33:49 +0000

    passthrough `retry_href` search param

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -9,14 +9,15 @@ export default function ErrorPage() {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const statusCode = Number(searchParams.get("status_code"))
+  const retryHref = searchParams.get("retry_href")
 
   if (isValidStatusCode(statusCode)) {
-    return <ServerErrorPage statusCode={statusCode.toString()} message={statusMessages[statusCode]} />
+    return <ServerErrorPage statusCode={statusCode.toString()} message={statusMessages[statusCode]} retryHref={retryHref} />
   }
 
   if (!Number.isNaN(statusCode)) {
-    return <ServerErrorPage statusCode={statusCode.toString()} message="Something strange..." />
+    return <ServerErrorPage statusCode={statusCode.toString()} message="Something strange..." retryHref={retryHref} />
   }
 
-  return <ServerErrorPage message="Something strange..." />
+  return <ServerErrorPage message="Something strange..." retryHref={retryHref} />
 }
