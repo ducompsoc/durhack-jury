b813fbd9f64f94fc74287a11f3050a63c78c32d2
dtft20 <dtft20@durham.ac.uk>
2026-03-30 15:53:15 +0100

    Added styling for options and implemented POST

--- client/src/components/admin/tables/HidePopup.tsx
+++ client/src/components/admin/tables/HidePopup.tsx
@@ -1,4 +1,8 @@
+import { postRequest, getRequest } from "../../../api";
 import { Project } from "../../../types";
+import { useState } from "react";
+import { errorAlert } from "../../../util";
+import { set } from "react-hook-form";
 
 interface HideProjectPopupProps {
     /* Project to edit */
@@ -9,26 +13,67 @@ interface HideProjectPopupProps {
 }
 
 const HideProjectPopup = ({ project, close }: HideProjectPopupProps) => {
-    const options = ['Guild sent to lunch', 'Team not found', 'Other'];
+    const [selectedOption, setSelectedOption] = useState('');
+    const [selectedReason, setSelectedReason] = useState('');
+    const options = ['Lunch', 'Not found', 'Option 3', 'Other'];
+
+    const hideProject = async () => {
+        if (selectedReason == '' || selectedReason == 'Other') {
+            alert('Please select a reason for hiding the project.');
+            return;
+        }
+        const res = await postRequest('/project/hide', 
+            { 
+                id: project.id, 
+                reason: selectedReason 
+            }
+        );
+        if (res.status === 200) {
+            alert(`Project hidden successfully!`);
+        }
+        else {
+            errorAlert(res);
+        }
+        close(false);
+    }
     return (
         <>
             <div className="bg-background fixed z-20 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] py-6 px-10 w-1/3">
                 <h1 className="text-5xl font-bold mb-2 text-center">Enter a reason for hiding {project.name}</h1>
-                <div className="flex flex-col">
-                    <ul className="list-disc list-inside">
-                        {options.map((option) => <li key={option}>{option}</li>)}
+                <div className="flex flex-row justify-around mt-4">
+                    <ul>
+                        {options.map((option) => 
+                            <div
+                                key={option}
+                                className={`max-w-sm rounded-md p-4 mg-12 gap-4 ${selectedOption == option ? 'bg-lightest' : 'bg-primary/20'}`}
+                                onClick={() => { 
+                                    setSelectedOption(option); 
+                                    setSelectedReason(option); 
+                                }}
+                            >
+                                {option}
+                            </div>
+                        )}
                     </ul>
+                </div>
+                <div className="bg-background flex flex-row mt-4">
+                    {selectedOption == 'Other' && 
+                    <textarea 
+                        className="border-lightest border-2 rounded-md p-2 resize-none w-full" 
+                        placeholder="Enter reason here..." 
+                        onChange={(e) => { setSelectedReason(e.target.value); }}
+                    />}
                 </div>
                 <div className="flex flex-row justify-around">
                     <button
-                        className=" border-lightest border-2 rounded-full px-6 py-1 mt-4 w-2/5 font-bold text-2xl text-lighter hover:bg-lighter/30 duration-200"
+                        className="border-lightest border-2 rounded-full px-6 py-1 mt-4 w-2/5 font-bold text-2xl text-lighter hover:bg-lighter/30 duration-200"
                         onClick={() => close(false)}
                     >
                         Cancel
                     </button>
                     <button
                         className="bg-primary rounded-full px-6 py-1 mt-4 w-2/5 font-bold text-2xl text-background hover:bg-primary/80 duration-200"
-                        onClick={() => close(false)}
+                        onClick={hideProject}
                     >
                         Submit
                     </button>
